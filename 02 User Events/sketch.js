25ca2a7860a6006d1f0ffce9382301163f32f4e3
7003982-lab <7003982@spsd.sk.ca>
2025-09-11 12:24:15 -0600

    Update sketch.js

--- 02 User Events/sketch.js
+++ 02 User Events/sketch.js
@@ -6,6 +6,11 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// GLOBAL VARIABLES
+let circleColor = false;
+//   declaration  initialization
+
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -15,9 +20,20 @@ function draw() {
   challenge();    //coordinate system challenge
 }
 
+function keyPressed(){
+  //this is a special EVENT function, gets automatically called eanytime a keyboard button is pressed
+  print("key was pressed")
+
+  circleColor = !circleColor
+}
+
 function challenge(){
   //draw 5 hollow circles, in 4 corners amd the center
   noFill();
+
+  if(circleColor){  //circleColor ===
+    fill(255,0,0)
+  }
 
   //5 circles
   circle(0,0,50)
