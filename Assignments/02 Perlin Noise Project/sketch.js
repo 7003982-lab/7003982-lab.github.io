7f45349e509e70d624f9eb237777fe7c304f1a1c
7003982-lab <7003982@spsd.sk.ca>
2025-10-01 14:09:53 -0600

    Moving Terrain

--- Assignments/02 Perlin Noise Project/sketch.js
+++ Assignments/02 Perlin Noise Project/sketch.js
@@ -5,44 +5,62 @@
 // GLOBAL VARIABLES
 let rectWidth = 1;
 let noiseTime = 0;
+let noiseStart
 let noiseOff = 0.01;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  generateTerrain();
+  noiseStart = noiseTime;
 
 }
 
 function draw() {
-  //background(220);
-  fill(0);
-  text(rectWidth, 100, 100);
+  background(220);
+  generateTerrain();
+  
 }
 
 function keyPressed(){
   // Check if any key is being pressed
-  if (keyCode === LEFT_ARROW){
-    rectWidth -= 1;
+  if (keyCode === 37){
+    if (rectWidth > 1){
+      rectWidth -= 1;
+    }
+    
   }
-  if (keyCode === RIGHT_ARROW){
-    rectWidth += 1;
+  if (keyCode === 39){
+    if (rectWidth < 10){
+      rectWidth += 1;
+    }
+    
   }
 }
 
 function generateTerrain(){
   // Use a loop to generate and draw several rectangles side to side to look like 2D terrain
   rectMode(CORNERS);
+  highestPeak = 0
+  peakX = 0;
+  peakY = 0;
 
   for(let x=0; x< width; x+=rectWidth){
     // Generate random height
     // Change from random to noise
     rectHeight = noise(noiseTime);
     rectHeight = map(rectHeight, 0, 1, height*0.3, height*0.9);
+    
 
 
     // Calculate the upper-right corner of rect
     let x2 = x + rectWidth;
     let y2 = height - rectHeight;
+
+    // Check for highest peak
+    if (rectHeight > highestPeak){
+          highestPeak = rectHeight;
+          peakX = x2;
+          peakY = y2;
+        }
 
     // Draw the rectngle
     rect(x, height, x2, y2);
@@ -51,12 +69,12 @@ function generateTerrain(){
     noiseTime += noiseOff;
   }
 
+  // Set noiseTime to 1 frame after
+  noiseStart += noiseOff;
+  noiseTime = noiseStart;
 
+}
 
-
-
-
-  rectMode(CORNER);
-
+function drawFlag(x, y){
 
 }
---
+++ Demos/09 Locate Smallest Circle/sketch.js
@@ -0,0 +1,46 @@
+// Locate Smallest Circle
+// Adrian Chan
+// October 1, 2025
+
+// GLOBAL VARIABLES
+let NUM_CIRCLES = 40;
+let seed;
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  seed = random(100);
+}
+
+function draw() {
+  randomSeed(seed);
+  background(220);
+  drawCircles();
+}
+
+function drawCircles(){
+  // Draw some random circles
+  noFill();
+  let smallestDiameter = Infinity;
+  let smallestX = -1;
+  let smallestY = -1;
+
+  for (let i=0; i< NUM_CIRCLES; i++){
+  let x = random(0, width);
+  let y = random(0, height);
+  let d = random(20, 100);
+
+  if(d<smallestDiameter){
+    smallestDiameter = d;
+    smallestX = x;
+    smallestY = y;
+  }
+  circle(x, y, d);
+  }
+
+  // At this point we should have access to the smallest circle
+  fill(255, 0, 0);
+  circle(smallestX, smallestY, smallestDiameter);
+
+}
