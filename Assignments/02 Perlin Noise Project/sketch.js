08dd1cbcafbbebaf8469eb24534387a087591350
7003982-lab <7003982@spsd.sk.ca>
2025-10-01 20:47:09 -0600

    Created flag function and average line function

--- Assignments/02 Perlin Noise Project/sketch.js
+++ Assignments/02 Perlin Noise Project/sketch.js
@@ -11,10 +11,11 @@ let noiseOff = 0.01;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noiseStart = noiseTime;
-
+  stroke(0);
 }
 
 function draw() {
+  //frameRate(0.1);
   background(220);
   generateTerrain();
   
@@ -38,29 +39,35 @@ function keyPressed(){
 
 function generateTerrain(){
   // Use a loop to generate and draw several rectangles side to side to look like 2D terrain
+  stroke(0);
+  noFill();
   rectMode(CORNERS);
   highestPeak = 0
   peakX = 0;
   peakY = 0;
+  sumY = 0;
+  averageY = 0;
 
   for(let x=0; x< width; x+=rectWidth){
     // Generate random height
     // Change from random to noise
     rectHeight = noise(noiseTime);
-    rectHeight = map(rectHeight, 0, 1, height*0.3, height*0.9);
+    rectHeight = map(rectHeight, 0, 1, height*0.1, height*0.9);
     
-
-
     // Calculate the upper-right corner of rect
     let x2 = x + rectWidth;
     let y2 = height - rectHeight;
 
     // Check for highest peak
     if (rectHeight > highestPeak){
-          highestPeak = rectHeight;
-          peakX = x2;
-          peakY = y2;
-        }
+      highestPeak = rectHeight;
+      peakX = x2 + rectWidth/2 - 0.5;
+      peakY = y2;
+    }
+
+    // Calculate the average height of each peak in frame
+    sumY += rectHeight
+    averageY = sumY/x
 
     // Draw the rectngle
     rect(x, height, x2, y2);
@@ -69,6 +76,12 @@ function generateTerrain(){
     noiseTime += noiseOff;
   }
 
+  // Call the function to draw the average line
+  drawAverage(averageY)
+
+  // Call the function to draw flag on the peak
+  drawFlag(peakX, peakY);
+
   // Set noiseTime to 1 frame after
   noiseStart += noiseOff;
   noiseTime = noiseStart;
@@ -76,5 +89,20 @@ function generateTerrain(){
 }
 
 function drawFlag(x, y){
+  // Create the flag on the peak
+  rectMode(CORNER);
+  stroke(0);
+  fill(0);
+  rect(x, y, 1, height*-0.05);
+  noStroke();
+  fill(255, 0, 0)
+  triangle(x+1.5, y+height*-0.05, x+1.5, y+height*-0.03, x+width*0.03, y+height*-0.04);
+}
 
+function drawAverage(y){
+  // Create a line at the average height of all peaks
+  rectMode(CENTER);
+  noStroke();
+  fill(255, 0, 0, 75);
+  rect(width/2, height-y, width, height*0.01)
 }
